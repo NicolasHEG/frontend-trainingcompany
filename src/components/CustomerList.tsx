f73ee41f193315913467d5b49722531cb698650d
Nicolas <nicolas.ciriza@etu.hesge.ch>
2025-03-25 16:26:51 +0200

    Get data from API for customer list and training list

--- src/components/CustomerList.tsx
+++ src/components/CustomerList.tsx
@@ -1,7 +1,59 @@
+import { useEffect, useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import { AllCommunityModule, ModuleRegistry, ColDef, themeMaterial } from 'ag-grid-community';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
+type Customer = {
+    firstname: string;
+    lastname: string;
+    streetaddress: string;
+    postcode: string;
+    city: string;
+    email: string;
+    phone: string;
+};
+
 export default function CustomerList() {
-    return(
+
+    const [customers, setCustomers] = useState([]);
+
+    const [columnDefs] = useState<ColDef<Customer>[]>([
+		{ field: 'firstname', filter: true },
+		{ field: 'lastname', width: 150, filter: true },
+		{ field: 'streetaddress', width: 150, filter: true },
+		{ field: 'postcode', width: 120, filter: true },
+		{ field: 'city', width: 120, filter: true },
+		{ field: 'email', width: 150, filter: true },
+        { field: 'phone', width: 150, filter: true },
+	]);
+
+    useEffect(() => {
+        fetch(`${apiUrl}/customers`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error when fetching customers');
+                }
+                return response.json();
+            })
+            .then(data => setCustomers(data._embedded.customers))
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
         <>
-            <h1>List of customers</h1>
+            <div style={{ width: '90%', height: 500 }}>
+                <h2>Customers</h2>
+				<AgGridReact
+					columnDefs={columnDefs}
+					rowData={customers}
+					pagination={true}
+					paginationAutoPageSize={true}
+					theme={themeMaterial}
+				/>
+			</div>
         </>
     );
 }
--- src/components/TrainingList.tsx
+++ src/components/TrainingList.tsx
@@ -1,7 +1,53 @@
+import { useEffect, useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import { AllCommunityModule, ModuleRegistry, ColDef, themeMaterial } from 'ag-grid-community';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
+type Training = {
+    date: string;
+    duration: number;
+    activity: string;
+    customer: string;
+};
+
 export default function TrainingList() {
-    return(
+
+    const [trainings, setTrainings] = useState([]);
+
+    const [columnDefs] = useState<ColDef<Training>[]>([
+        { field: 'date', filter: true },
+        { field: 'duration', width: 150, filter: true },
+        { field: 'activity', width: 150, filter: true },
+        { field: 'customer', width: 120, filter: true },
+    ]);
+
+    useEffect(() => {
+        fetch(`${apiUrl}/trainings`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Error when fetching trainings');
+                }
+                return response.json();
+            })
+            .then(data => setTrainings(data._embedded.trainings))
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
         <>
             <h1>List of trainings</h1>
+            <div style={{ width: '90%', height: 500 }}>
+                <AgGridReact
+                    columnDefs={columnDefs}
+                    rowData={trainings}
+                    pagination={true}
+                    paginationAutoPageSize={true}
+                    theme={themeMaterial}
+                />
+            </div>
         </>
     );
 }
