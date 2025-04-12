66e84d5f63a2159896e75864e4e085f1b2c775bf
Nicolas <nicolas.ciriza@etu.hesge.ch>
2025-04-12 15:17:42 +0300

    Configure deployment on github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
 import CustomerList from './components/CustomerList.tsx'
 import TrainingList from './components/TrainingList.tsx'
 import TrainingCalendar from './components/TrainingCalendar.tsx'
 
-const router = createBrowserRouter([
-  {
-    // The root path ("/") renders the App component as the main layout.
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        // The default child route renders the CustomerList component.
-        element: <CustomerList />,
-        index: true
-      },
-      {
-        path: "/trainings",
-        element: <TrainingList />
-      },
-      {
-        path: "/calendar",
-        element: <TrainingCalendar />
-      }
-    ]
-  }
-])
+const router = createHashRouter(
+  [
+    {
+      // The root path ("/") renders the App component as the main layout.
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          // The default child route renders the CustomerList component.
+          element: <CustomerList />,
+          index: true
+        },
+        {
+          path: "/trainings",
+          element: <TrainingList />
+        },
+        {
+          path: "/calendar",
+          element: <TrainingCalendar />
+        }
+      ],
+    },
+  ]
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
